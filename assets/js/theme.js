f4179356c40328a91fde836f0feb1aaaee8c0c33
catarinaleitao20 <catarina.leitao.dengun@gmail.com>
2019-03-25 17:20:08 +0000

    adicionados novos temas

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,43 +1,32 @@
-function changeTheme(){
-	document.body.className='theme-purple';
+function changeTheme(theme){
+	document.body.className=theme;
 
 }
+
 
 //Podia usar também document.queryselector
 let button = document.getElementsByClassName('theme-button')[0];
-button.addEventListener('click', changeTheme);
-
-function changeThemeRed(){
-	document.body.className='theme-red';
-
-}
+button.addEventListener('click', function(){
+	changeTheme('theme-purple');
+});
 
 //Podia usar também document.queryselector
 let red = document.getElementsByClassName('theme-button-red')[0];
-red.addEventListener('click', changeThemeRed);
-
-
-function changeThemeGreen(){
-	document.body.className='theme-green';
-}
-
+red.addEventListener('click', function(){
+	changeTheme('theme-red');
+});
 
 let green = document.getElementsByClassName('theme-button-green')[0];
-green.addEventListener('click', changeThemeGreen);
-
-
-function changeThemeBlue(){
-	document.body.className='theme-blue';
-}
-
+green.addEventListener('click', function(){
+	changeTheme('theme-green');
+});
 
 let blue = document.getElementsByClassName('theme-button-blue')[0];
-blue.addEventListener('click', changeThemeBlue);
-
-function changeThemeYellow(){
-	document.body.className='theme-yellow';
-}
-
+blue.addEventListener('click', function(){
+	changeTheme('theme-blue');
+});
 
 let yellow = document.getElementsByClassName('theme-button-yellow')[0];
-yellow.addEventListener('click', changeThemeYellow);
+yellow.addEventListener('click', function(){
+	changeTheme('theme-yellow');
+});
